f59e2097950e1df43c007f3bb171b8e0ba657ab7
mzathem <mzanthem@gmail.com>
2018-09-06 19:11:47 +0800

    Site updated: 2018-09-06 19:11:46

--- js/src/motion.js
+++ js/src/motion.js
@@ -1,6 +1,21 @@
 /* global NexT: true */
 
 $(document).ready(function () {
+  //fix left sidebar bug
+  window.onload=function(){
+    var bwol=document.body.offsetWidth;
+    if(bwol < 975){
+        $('aside#sidebar').css("display","none");
+        $('body').css("paddingLeft","0px");
+    }
+  }
+  window.onresize = function(){
+    var bwos=document.body.offsetWidth;
+    bwos < 975 && $('body').velocity('stop').velocity({paddingLeft: 0},0);
+    if($('aside#sidebar').css('display') != 'none' && $('aside#sidebar').css('width')!='0px')
+        $('body').velocity('stop').velocity({paddingLeft: 350},0);
+  }
+
   NexT.motion = {};
 
   var sidebarToggleLines = {
@@ -55,21 +70,24 @@ $(document).ready(function () {
     el: '.sidebar-toggle-line-first',
     status: {
       arrow: {width: '50%', rotateZ: '-45deg', top: '2px'},
-      close: {width: '100%', rotateZ: '-45deg', top: '5px'}
+      // close: {width: '100%', rotateZ: '-45deg', top: '5px'} //叉形头
+      close: {width: '50%', rotateZ: '-45deg', top: '2px', left: '0px'} //箭头形
     }
   });
   var sidebarToggleLine2nd = new SidebarToggleLine({
     el: '.sidebar-toggle-line-middle',
     status: {
       arrow: {width: '90%'},
-      close: {opacity: 0}
+      // close: {opacity: 0}
+      close: {width: '90%'}
     }
   });
   var sidebarToggleLine3rd = new SidebarToggleLine({
     el: '.sidebar-toggle-line-last',
     status: {
       arrow: {width: '50%', rotateZ: '45deg', top: '-2px'},
-      close: {width: '100%', rotateZ: '45deg', top: '-5px'}
+      // close: {width: '100%', rotateZ: '45deg', top: '-5px'}
+      close: {width: '50%', rotateZ: '45deg', top: '-2px', left: '0px'} //箭头形
     }
   });
 
@@ -98,7 +116,7 @@ $(document).ready(function () {
       $(document)
         .on('sidebar.isShowing', function () {
           NexT.utils.isDesktop() && $('body').velocity('stop').velocity(
-            {paddingRight: SIDEBAR_WIDTH},
+            {paddingLeft: SIDEBAR_WIDTH},
             SIDEBAR_DISPLAY_DURATION
           );
         })
@@ -164,7 +182,7 @@ $(document).ready(function () {
       this.sidebarEl.trigger('sidebar.isShowing');
     },
     hideSidebar: function () {
-      NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingRight: 0});
+      NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingLeft: 0});
       this.sidebarEl.find('.motion-element').velocity('stop').css('display', 'none');
       this.sidebarEl.velocity('stop').velocity({width: 0}, {display: 'none'});
 
